dd4ba6fa6f767e607996cd817dbd5ccdf80e5be0
unknown <kudaibergenov.arystan@fizmat.kz>
2023-01-09 11:17:17 +0600

    lesson_1

---
+++ client/src/App.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+
+function App() {
+  return (
+    <div>Happy Hacking!</div>
+  );
+}
+
+setTimeout(() => {
+  console.log("hello with delay 2s")
+}, 2000)
+
+
+export default App;
